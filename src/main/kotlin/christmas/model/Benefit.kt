11a4c2de1ba075d68654507957bd8994544fa367
dpcks0509 <dpcks0509@gmail.com>
2023-12-13 23:10:41 +0900

    fix: 총주문 금액 10000 이하 이벤트 혜택 적용 버그 수정
    총주문 금액이 10000 이하일 경우에도 이벤트 혜택이 적용되는 버그를 수정하였다.

--- src/main/kotlin/christmas/model/Benefit.kt
+++ src/main/kotlin/christmas/model/Benefit.kt
@@ -1,20 +1,21 @@
 package christmas.model
 
 import christmas.model.Badge.Companion.getBadgeByTotalBenefitAmount
+import christmas.model.Menu.Companion.getMenuPriceByFood
 import christmas.utils.Constants.BENEFIT_MIN_AMOUNT
 
-class Benefit(private val visitDay: Int, private val orders: List<Order>) {
+class Benefit(visitDay: Int, private val orders: List<Order>) {
     private val totalOrderAmount = calculateTotalOrderAmount()
     private val giveAway = GiveAway(totalOrderAmount)
-    private val discount = Discount(visitDay, orders)
+    private val discount = Discount(visitDay, orders).takeIf { totalOrderAmount >= BENEFIT_MIN_AMOUNT }
+        ?: Discount(0, emptyList())
     private val totalBenefitAmount = calculateTotalBenefitAmount()
     private val paymentAmount = calculatePaymentAmount()
-    private val badge = getBadgeByTotalBenefitAmount(totalBenefitAmount)
+    private val badge = getBadgeByTotalBenefitAmount(totalBenefitAmount).takeIf { totalOrderAmount >= BENEFIT_MIN_AMOUNT }
+        ?: getBadgeByTotalBenefitAmount(0)
 
     private fun calculateTotalOrderAmount(): Int {
-        return orders.sumOf { order ->
-            Menu.getMenuPriceByFood(order.getFood()) * order.getQuantity()
-        }
+        return orders.sumOf { order -> getMenuPriceByFood(order.getFood()) * order.getQuantity() }
     }
 
     private fun calculateTotalBenefitAmount(): Int {
@@ -27,8 +28,8 @@ class Benefit(private val visitDay: Int, private val orders: List<Order>) {
 
     fun getTotalOrderAmount() = totalOrderAmount
     fun getGiveAway() = giveAway
-    fun getDiscount() = discount.takeIf { totalOrderAmount >= BENEFIT_MIN_AMOUNT } ?: Discount(0, emptyList())
+    fun getDiscount() = discount
     fun getTotalBenefitAmount() = totalBenefitAmount
     fun getPaymentAmount() = paymentAmount
-    fun getBadge() = badge.takeIf { totalOrderAmount >= BENEFIT_MIN_AMOUNT } ?: getBadgeByTotalBenefitAmount(0)
+    fun getBadge() = badge
 }
