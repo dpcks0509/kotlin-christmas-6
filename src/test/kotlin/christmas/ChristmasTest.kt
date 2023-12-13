ce45ebd8a41dbdfc070f9e4ee24e469fdd865622
dpcks0509 <dpcks0509@gmail.com>
2023-12-13 23:42:14 +0900

    test: 증정 메뉴 증정 판단 테스트 추가
    증정 메뉴 증정을 판단하는 테스트를 추가하였다.

--- src/test/kotlin/christmas/ChristmasTest.kt
+++ src/test/kotlin/christmas/ChristmasTest.kt
@@ -75,4 +75,15 @@ class ChristmasTest {
 
         assertThat(actualTotalOrderAmount).isEqualTo(expectTotalOrderAmount)
     }
+
+    @ParameterizedTest
+    @CsvSource("26:타파스-1,제로콜라-1:없음", "3:티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:샴페인 1개" ,delimiter = ':')
+    fun `증정 메뉴 증정 판단`(visitDay: Int, inputOrders: String, expectGiveAway: String) {
+        val orders = validateOrders(inputOrders)
+        val benefit = Benefit(visitDay, orders)
+
+        val actualGiveAway = benefit.getGiveAway().toString()
+
+        assertThat(actualGiveAway).isEqualTo(expectGiveAway)
+    }
 }
